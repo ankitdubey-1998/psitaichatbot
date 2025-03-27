f596ffc9483f7285c7d70aac9e2837698cc19cf7
ankit dubey <ankdev1998@gmail.com>
2025-03-27 17:20:00 +0530

    init

---
+++ script.js
@@ -0,0 +1,77 @@
+const chatBody = document.querySelector(".chat-body");
+const messageInput = document.querySelector(".message-input");
+const sendMessageButton = document.querySelector("#send-message");
+
+const API_URL = ``;
+
+const userData = {
+    message: null
+}
+
+//Create message element with dynamic classes and return it
+const createMessageElement = (content,...classes) => {
+const div = document.createElement("div");
+div.classList.add("message",...classes);
+div.innerHTML = content;
+return div;
+}
+
+const generateBotResponse = () => {
+
+};
+
+
+//Handle outgoing user messages 
+const handleOutgoingMessage = (e) => {
+    e.preventDefault();
+
+    userData.message = messageInput.value.trim();
+    messageInput.value = "";
+
+    //Create and display user message 
+const messageContent =  `<div class="message-text">
+</div>`;
+
+const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
+outgoingMessageDiv.querySelector(".message-text").textContent = userData.message;
+chatBody.appendChild(outgoingMessageDiv);
+
+//Simulate bot response with thinking indicator after a delay
+setTimeout(() => {
+    const messageContent =  `<svg
+            class="bot-avatar"
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            viewBox="0 0 1024 1024"
+          >
+            <path
+              d="M738.3 287.6H285.7c-59 0-106.8 47.8-106.8 106.8v303.1c0 59 47.8 106.8 106.8 106.8h81.5v111.1c0 .7.8 1.1 1.4.7l166.9-110.6 41.8-.8h117.4l43.6-.4c59 0 106.8-47.8 106.8-106.8V394.5c0-59-47.8-106.9-106.8-106.9zM351.7 448.2c0-29.5 23.9-53.5 53.5-53.5s53.5 23.9 53.5 53.5-23.9 53.5-53.5 53.5-53.5-23.9-53.5-53.5zm157.9 267.1c-67.8 0-123.8-47.5-132.3-109h264.6c-8.6 61.5-64.5 109-132.3 109zm110-213.7c-29.5 0-53.5-23.9-53.5-53.5s23.9-53.5 53.5-53.5 53.5 23.9 53.5 53.5-23.9 53.5-53.5 53.5z"
+            ></path>
+          </svg>
+          <div class="message-text">
+            <div class="thinking-indicator">
+              <div class="dot"></div>
+              <div class="dot"></div>
+              <div class="dot"></div>
+            </div>
+          </div>`;
+
+const incomingMessageDiv = createMessageElement(messageContent, "bot-message", "thinking");
+chatBody.appendChild(incomingMessageDiv);
+generateBotResponse();
+}, 600);
+}
+
+
+//Handle Enter key press for sending messages
+messageInput.addEventListener("keydown",(e)=>{
+    const userMessage = e.target.value.trim();
+    if(e.key === "Enter" && userMessage){
+        e.preventDefault();
+        handleOutgoingMessage(e);
+        messageInput.value = "";
+    }
+})
+
+sendMessageButton.addEventListener("click",(e)=> handleOutgoingMessage(e));
